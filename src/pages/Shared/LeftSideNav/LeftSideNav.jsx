56d3e1a421e61d78087fea00ffd3f320d1ca3cd5
ammmar <ammaraslam7164@gmail.com>
2024-03-09 21:24:05 +0600

    moemnt-js-time-added

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import moment from 'moment';
+import  image1 from "../../../assets/1.png"
+import  image2 from "../../../assets/2.png"
+import  image3 from "../../../assets/3.png"
+
+
 
 const LeftSideNav = () => {
   const [categories, setCatgories] = useState([]);
@@ -15,12 +21,36 @@ const LeftSideNav = () => {
 
       {categories.map((element) => (
         <NavLink
-          className="text-[#9F9F9F] block text-[20px] mb-3 ml-4"
+          className="no-underline text-[#9F9F9F] block text-[20px] mb-3 ml-4"
           key={element.id}
+          to={`/category/${element.id}`}
         >
           {element.name}
         </NavLink>
       ))}
+
+      <div>
+
+          <img src={image1} alt="" />
+          <p className="text-center font-bold">Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+           <div className="flex gap-10 ml-6">
+            <p>sports</p>
+            <p>{moment().format("MMMM Do YYYY")} </p>
+           </div>
+          <img src={image2} alt="" />
+          <p className="text-center font-bold">Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+           <div className="flex gap-10 ml-6">
+            <p>sports</p>
+            <p>{moment().format("MMMM Do YYYY")} </p>
+           </div>
+          <img src={image3} alt="" />
+          <p className="text-center font-bold">Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+           <div className="flex gap-10 ml-6">
+            <p>sports</p>
+            <p>{moment().format("MMMM Do YYYY")} </p>
+           </div>
+
+      </div>
     </div>
   );
 };
