133d46830cde17fbad018a5b6ce6c6f778d9b52e
ammmar <ammaraslam7164@gmail.com>
2024-03-10 01:09:42 +0600

    firebase-auth-setted

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Routes.jsx'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import router from "./Routes/Routes.jsx";
+import AuthProvider from "./pages/Shared/providers/AuthProvider.jsx";
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider >
+      <RouterProvider router={router}></RouterProvider>
+
+   
+    </AuthProvider>
+  </React.StrictMode>
+);
